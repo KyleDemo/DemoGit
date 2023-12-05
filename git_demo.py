231d87c2ceeab30157a8e63bd93b422132bc8fce
KyleDemo <153006353+KyleDemo@users.noreply.github.com>
2023-12-05 09:48:20 -0700

    Create git_demo.py
    Added file

---
+++ git_demo.py
@@ -0,0 +1,12 @@
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
+
+def filter_by_role(session, table_name, role):
+  df = session.table(table_name)
+  return df.filter(col("role") == role)
+
+def main(session: snowpark.Session): 
+    filter_by_role(session, table_name, role)
